8fb43748ee337c673f5ed6ac6e579c88717601ad
Samuel Maina <samuelmayna@gmail.com>
2023-07-20 19:21:32 +0000

    changed of the model folder and creating one model function

---
+++ model/framework/code/main.py
@@ -0,0 +1,35 @@
+# imports
+import os
+import csv
+import sys
+from rdkit import Chem
+from rdkit.Chem.Descriptors import MolWt
+
+from grover.main import grover_predict
+
+# parse arguments
+input_file = sys.argv[1]
+output_file = sys.argv[2]
+
+# current file directory
+root = os.path.dirname(os.path.abspath(__file__))
+
+# my model
+
+
+
+
+# run model. No need to read the smiles from the input csv file, the model will read and return the smiles_list
+outputs, smiles_list = grover_predict(input_txt_path=input_file, output_path=output_file)
+
+#check input and output have the same lenght
+input_len = len(smiles_list)
+output_len = len(outputs)
+assert input_len == output_len
+
+# write output in a .csv file
+with open(output_file, "w") as f:
+    writer = csv.writer(f)
+    writer.writerow(["value"])  # header
+    for o in outputs:
+        writer.writerow([o])
